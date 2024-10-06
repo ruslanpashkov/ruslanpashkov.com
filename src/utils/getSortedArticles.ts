cea5ceef9c8e9c98e3ba179f7a5eef68697f819d
Ruslan <hi@ruslanpashkov.com>
2024-10-06 18:42:24 +0300

    fix: Replace .toSorted method with good old sort

--- src/utils/getSortedArticles.ts
+++ src/utils/getSortedArticles.ts
@@ -1,7 +1,7 @@
 import type { Article } from '@/types/Article';
 
 export function getSortedArticles(articles: Article[]): Article[] {
-	return articles.toSorted((firstArticle, secondArticle) => {
+	return [...articles].sort((firstArticle, secondArticle) => {
 		const firstDate = new Date(firstArticle.data.pubDate);
 		const secondDate = new Date(secondArticle.data.pubDate);
 
