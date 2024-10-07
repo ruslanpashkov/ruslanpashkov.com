6bccb69c99e96faef99ca17d5a6e1c25bf2bbbf2
Ruslan <hi@ruslanpashkov.com>
2024-10-07 22:27:47 +0300

    feat: Remove Astro Shield integration and add Netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import sitemap from '@astrojs/sitemap';
-import { shield } from '@kindspells/astro-shield';
 import Compress from '@playform/compress';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
@@ -7,11 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
-		shield({
-			securityHeaders: {
-				enableOnStaticPages: { provider: 'netlify' },
-			},
-		}),
 		sitemap({ changefreq: 'weekly' }),
 		icon({ iconDir: 'src/assets/svg' }),
 		Compress(),
