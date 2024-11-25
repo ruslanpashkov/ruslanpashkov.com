4f5d8f5d352b12170e5fd62ccf10eb7a9c0cbc53
Ruslan <hi@ruslanpashkov.com>
2024-11-25 02:02:59 +0200

    chore: Update project link

--- redirects.ts
+++ redirects.ts
@@ -2,6 +2,7 @@ export default {
 	'/projects/just-one-more/': 'https://justonemore.click/',
 	'/projects/met-gallery/': 'https://met-gallery.vercel.app/',
 	'/projects/nice-gadgets/': 'https://nice-gadgets.vercel.app/',
-	'/projects/novapass/': 'https://addons.mozilla.org/en-US/firefox/addon/novapass/',
+	'/projects/novapass/':
+		'https://chromewebstore.google.com/detail/jpooemhlkehmepkbcbjflhpnjgilellm/',
 	'/projects/todomvc-clone/': 'https://todomvc-clone.vercel.app/',
 };
