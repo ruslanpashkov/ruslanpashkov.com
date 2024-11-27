49da9a86953920dabffa8629049768eb6f4899a6
Ruslan <hi@ruslanpashkov.com>
2024-11-27 14:41:49 +0200

    chore: Update project description

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -19,7 +19,7 @@ export const projects: Project[] = [
 	},
 	{
 		description:
-			"Product catalog with Apple products and a shopping cart. Do we need more new iPhones in database? I don't think so. Simple but stylish.",
+			"Led a team of 5 devs to build a product catalog for Apple stuff with a cart. Another iPhone model? Please, we already have enough in our DB.",
 		tags: ['TypeScript', 'React', 'SCSS', 'Express.js', 'PostgreSQL', 'Sequelize'],
 		title: 'Nice Gadgets',
 		type: 'Web Showcases',
