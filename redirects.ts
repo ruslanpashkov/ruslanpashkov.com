ebbf0052b8c32ef5f3efbc632f668bf4536da967
Ruslan <hi@ruslanpashkov.com>
2024-12-02 16:43:20 +0200

    chore: Update project link

--- redirects.ts
+++ redirects.ts
@@ -1,7 +1,7 @@
 export default {
 	'/projects/just-one-more/': 'https://justonemore.click/',
 	'/projects/met-exhibition/': 'https://met-exhibition.netlify.app/',
-	'/projects/nice-gadgets/': 'https://nice-gadgets.netlify.app/',
+	'/projects/nice-gadgets/': 'https://nice-gadgetz.netlify.app/',
 	'/projects/novapass/':
 		'https://chromewebstore.google.com/detail/jpooemhlkehmepkbcbjflhpnjgilellm/',
 	'/projects/todomvc-clone/': 'https://todomvc-clone.netlify.app/',
