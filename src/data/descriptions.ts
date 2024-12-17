53a6234c1ded58c831fb0c7e4f0248add03315a5
Ruslan <hi@ruslanpashkov.com>
2024-12-17 02:23:46 +0200

    chore: Update page metadata descriptions

--- src/data/descriptions.ts
+++ src/data/descriptions.ts
@@ -1,7 +1,7 @@
 export const descriptions = {
 	articles:
-		'Explore in-depth articles on software development, JavaScript best practices, and cutting-edge web technologies.',
-	home: 'Software Engineer, JavaScript enthusiast and Producer. Currently working at Wiew as a Lead Frontend Engineer.',
+		'Explore in-depth articles on software development, JavaScript best practices, and cutting-edge web technologies. Sometimes I write about centering divs, sometimes about building spaceships with vanilla JS.',
+	home: 'Hi, I am Ruslan Pashkov! Software Engineer, JavaScript enthusiast and Producer. Currently working at Wiew as a Lead Frontend Engineer. Making browsers do things they were not meant to do.',
 	projects:
-		'Explore my portfolio of projects, which includes web applications, websites, utilities, and developer tools.',
+		'Explore my portfolio of projects, which includes web applications, websites, utilities, and developer tools. From playful web showcases to practical utilities, each project has its own story.',
 };
