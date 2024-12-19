f2ab03c995a4a09c675660e3c4c4e4b135023f3e
Ruslan <hi@ruslanpashkov.com>
2024-12-19 03:10:07 +0200

    feat: Add reading time and .mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,29 @@
+import mdx from '@astrojs/mdx';
 import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import Compress from '@playform/compress';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
+import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import rehypeExternalLinks from 'rehype-external-links';
 import rehypeSlug from 'rehype-slug';
 
 import redirects from './redirects';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
 	adapter: netlify(),
 	integrations: [
+		mdx(),
 		sitemap({ changefreq: 'weekly' }),
 		icon({ iconDir: 'src/assets/svg' }),
 		Compress({ CSS: false }),
 	],
 	markdown: {
 		rehypePlugins: [
+			rehypeAccessibleEmojis,
 			rehypeSlug,
 			[
 				rehypeExternalLinks,
@@ -31,14 +36,24 @@ export default defineConfig({
 				rehypeAutolinkHeadings,
 				{
 					behavior: 'append',
-					properties: {
-						ariaHidden: 'true',
-						className: ['heading-link'],
-						tabIndex: -1,
+					content: {
+						children: [{ type: 'text', value: '#' }],
+						properties: {
+							'aria-hidden': 'true',
+							className: ['heading-link-icon'],
+						},
+						tagName: 'span',
+						type: 'element',
 					},
+					properties: (heading) => ({
+						'aria-label': `Navigate to ${heading.children[0].value}`,
+						className: ['heading-link', 'hover'],
+						tabIndex: -1,
+					}),
 				},
 			],
 		],
+		remarkPlugins: [remarkReadingTime],
 		shikiConfig: {
 			themes: {
 				dark: 'github-dark',
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,17 @@
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
+	loader: glob({ base: 'src/content/blog', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
 		categories: z.array(z.string()),
 		description: z.string(),
 		publishedAt: z.string(),
+		slug: z.string(),
 		title: z.string(),
 		topic: z.string(),
 		updatedAt: z.optional(z.string()),
 	}),
-	type: 'content',
 });
 
 export const collections = { blog };
