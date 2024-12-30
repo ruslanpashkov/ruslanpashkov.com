ba4bba928284be5c334609a92396245bb7e2a33a
Ruslan <hi@ruslanpashkov.com>
2024-12-30 23:51:16 +0200

    feat: Make footer licenses as a links

--- src/data/copyright.ts
+++ src/data/copyright.ts
@@ -1,2 +1,24 @@
-export const copyright =
-	'© 2024 Ruslan Pashkov. Code: MIT. Content: CC BY-NC-SA 4.0. Fonts: SIL OFL.';
+import type { Link } from '@/types/Link.ts';
+
+export const licenses: Link[] = [
+	{
+		label: 'Code',
+		title: 'MIT',
+		url: 'https://github.com/ruslanpashkov/ruslanpashkov.com/blob/main/LICENSE',
+	},
+	{
+		label: 'Content',
+		title: 'CC BY-NC-SA 4.0',
+		url: 'https://github.com/ruslanpashkov/ruslanpashkov.com/blob/main/CC.md',
+	},
+	{
+		label: 'Fonts',
+		title: 'SIL OFL',
+		url: 'https://github.com/ruslanpashkov/ruslanpashkov.com/blob/main/OFL.md',
+	},
+];
+
+export const copyright = {
+	licenses,
+	year: 2024,
+};
