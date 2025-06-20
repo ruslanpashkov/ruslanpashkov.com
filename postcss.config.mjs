89819faf81ab96e60c029c96294d688f1af62637
Ruslan <hi@ruslanpashkov.com>
2025-06-20 21:17:23 +0300

    chore: Add postcss-load-config dependency and update configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
 import autoprefixer from 'autoprefixer';
 import postcssPresetEnv from 'postcss-preset-env';
 
+/** @type {import('postcss-load-config').Config} */
 export default {
 	plugins: [autoprefixer(), postcssPresetEnv()],
 };
