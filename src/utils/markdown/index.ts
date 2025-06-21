b2e51dc932832428ce740277458060265ffb5ac2
Ruslan <hi@ruslanpashkov.com>
2025-06-21 05:45:23 +0300

    refactor: Simplify snippet cleaning and newline handling

--- src/utils/markdown/index.ts
+++ src/utils/markdown/index.ts
@@ -1,22 +1,14 @@
 import removeMarkdown from 'remove-markdown';
 
-export const cleanSnippets = (markdown: string): string => {
-	const startMarker = '{/* snippet:start */}';
-	const endMarker = '{/* snippet:end */}';
+export const cleanSnippets = (markdown: string): string =>
+	markdown
+		.replace(/\{\/\* snippet:start \*\/\}[\s\S]*?\{\/\* snippet:end \*\/\}/g, '')
+		.replace(/\n{3,}/g, '\n\n');
 
-	const startIndex = markdown.indexOf(startMarker);
-	const endIndex = markdown.indexOf(endMarker);
+export const cleanNewlines = (text: string): string =>
+	text.replace(/\n+/g, ' ').replace(/\s+/g, ' ').trim();
 
-	if (startIndex === -1 || endIndex === -1) {
-		return markdown;
-	}
-
-	const beforeImports = markdown.substring(0, startIndex);
-	const afterImports = markdown.substring(endIndex + endMarker.length);
-
-	return (beforeImports + afterImports).replace(/\n{3,}/g, '\n\n');
-};
-
-export const cleanContent = (markdown: string): string => removeMarkdown(markdown);
+export const cleanContent = (markdown: string): string =>
+	cleanNewlines(removeMarkdown(markdown.trim()));
 
 export const clean = (markdown: string): string => cleanContent(cleanSnippets(markdown));
