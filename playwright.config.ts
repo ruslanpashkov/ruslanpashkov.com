a86bc8c1e81483de7483321dc8c728842b570375
Ruslan <hi@ruslanpashkov.com>
2025-06-24 22:40:26 +0300

    test: Remove Firefox from Playwright setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,9 +4,8 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	fullyParallel: true,
 	projects: [
-		{ name: 'Chromium', use: { ...devices['Desktop Chrome'] } },
-		{ name: 'Firefox', use: { ...devices['Desktop Firefox'] } },
-		{ name: 'WebKit', use: { ...devices['Desktop Safari'] } },
+		{ name: 'Desktop Chrome', use: { ...devices['Desktop Chrome'] } },
+		{ name: 'Desktop Safari', use: { ...devices['Desktop Safari'] } },
 		{ name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
 		{ name: 'Mobile Safari', use: { ...devices['iPhone 12'] } },
 	],
