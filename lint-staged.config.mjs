1766149201092976e760cd3a468af150248b314b
Ruslan <hi@ruslanpashkov.com>
2025-06-26 22:12:02 +0300

    chore: Update lint-staged configuration and pre-push hook

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-	'*': 'bun run check',
+	'*': 'bun run format:check',
+	'*.{astro,ts}': 'bun run typecheck',
+	'*.{astro,js,mjs,ts}': 'bun run lint:eslint',
+	'*.{astro,css}': 'bun run lint:stylelint',
 };
