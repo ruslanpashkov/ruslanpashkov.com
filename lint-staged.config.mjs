5326a8b309202b6ba03d6ec26e9119216cd14854
Ruslan <hi@ruslanpashkov.com>
2025-06-27 15:04:04 +0300

    chore: Update lint-staged configuration to use auto-fix

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-	'*.{astro,js,mjs,ts,css,md,mdx,json,yml,webmanifest}': 'bun run format:check',
-	'*.{astro,js,mjs,ts}': 'bun run lint:eslint',
-	'*.{astro,css}': 'bun run lint:stylelint',
+	'*.{astro,js,mjs,ts,css,md,mdx,json,yml,webmanifest}': 'prettier --write',
+	'*.{astro,js,mjs,ts}': 'eslint --fix',
+	'*.{astro,css}': 'stylelint --fix',
 };
