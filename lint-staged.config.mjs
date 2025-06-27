52e91d2ea849890a61c935411eaa24966b319b04
Ruslan <hi@ruslanpashkov.com>
2025-06-27 12:52:22 +0300

    chore: Specify file types for Prettier formatter

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
-	'*': 'bun run format:check',
+	'*.{astro,js,mjs,ts,css,md,mdx,json,yml,webmanifest}': 'bun run format:check',
 	'*.{astro,js,mjs,ts}': 'bun run lint:eslint',
 	'*.{astro,css}': 'bun run lint:stylelint',
 };
