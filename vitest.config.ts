d0eee2593b57cc39be187a902c4ec8226cb3a989
Ruslan <hi@ruslanpashkov.com>
2025-12-30 06:56:28 +0100

    test: Add thresholds for coverage and fix typing issue

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,7 @@
-/// <reference types="vitest" />
+/// <reference types="vitest/config" />
 import { getViteConfig } from 'astro/config';
 
 export default getViteConfig({
-	// @ts-expect-error - Astro's getViteConfig types don't include Vitest's test property
 	test: {
 		include: ['tests/vitest/**/*.test.ts'],
 		reporters: ['default', 'html'],
@@ -11,10 +10,16 @@ export default getViteConfig({
 		},
 		environment: 'jsdom',
 		coverage: {
-			include: ['src/**/*.ts'],
+			include: ['src/utils/**/*.ts'],
 			reporter: ['text', 'json', 'html'],
 			reportsDirectory: 'test-reports/vitest/coverage',
 			provider: 'v8',
+			thresholds: {
+				statements: 90,
+				branches: 90,
+				functions: 90,
+				lines: 90,
+			},
 		},
 	},
 });
